2e1db467199d5765055b7f3308544ed14f23f3a7
Bomou He <paakmau@outlook.com>
2022-01-17 00:27:55 +0800

    Add a static library target

--- xmake.lua
+++ xmake.lua
@@ -7,3 +7,9 @@ set_warnings("all", "error")
 set_languages("cxx20")
 
 add_rules("mode.debug", "mode.release")
+
+target("Demo")
+    set_kind("static")
+    add_files("src/*.cpp")
+    add_includedirs("include", { public = true })
+    add_headerfiles("include/(Demo/*.h)")
